9607f62219359b0ae9777fc0ecfc0691dc780e3d
Gustavo Henrique Xavier da Silva <GustavodaSilva@users.noreply.github.com>
2016-03-28 20:59:02 -0300

    Atualização da superclasse funcionário
    Atualização com a inserção de variáveis na superclasse funcionário.

--- EmpresaTransporte/src/empresatransporte/Funcionario.java
+++ EmpresaTransporte/src/empresatransporte/Funcionario.java
@@ -2,5 +2,23 @@
 package empresatransporte;
 
 public class Funcionario {
+    String nome;
+    int cpf;
+    int ctps;
+    String data_nascimento;
+    String estado_civil;
+    int dependentes;
+    char sexo;
+    double salario;
+    String tipo_salario;
+    double horas_trabalhadas;
+    String cargo;
+  
     
+    
+    public Funcionario(String n, double s){
+        this.nome = n;
+        this.salario = s;
+        
+    }
 }
