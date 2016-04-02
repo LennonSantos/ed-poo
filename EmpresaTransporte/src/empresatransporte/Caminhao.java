93134fd49e30802557f46ab7c2c9a7a1f153efd4
Lennon Santos <lennonsbueno@gmail.com>
2016-04-01 23:47:03 -0300

    extends veiculo

--- EmpresaTransporte/src/empresatransporte/Caminhao.java
+++ EmpresaTransporte/src/empresatransporte/Caminhao.java
@@ -1,5 +1,7 @@
 package empresatransporte;
 
-public class Caminhao {
-    
+public class Caminhao extends Veiculo{
+    public Caminhao(String mod, String mot, int anoF, int anoM, String cor, String com, double val){
+      super(mod, mot, anoF, anoM, cor, com, val);
+    }
 }
--- EmpresaTransporte/src/empresatransporte/Carro.java
+++ EmpresaTransporte/src/empresatransporte/Carro.java
@@ -1,5 +1,9 @@
 package empresatransporte;
 
-public class Carro {
+public class Carro extends Veiculo{
+    
+    public Carro(String mod, String mot, int anoF, int anoM, String cor, String com, double val){
+        super(mod, mot, anoF, anoM, cor, com, val);
+    }
     
 }
